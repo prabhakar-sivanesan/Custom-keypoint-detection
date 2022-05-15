8080a2a700ae3140d3d5535fd9362ffeec7d6fa0
Prabhakar Sivanesan <sivanesanprabhakar@gmail.com>
2022-05-16 01:42:48 +0530

    added inference scripts

--- inference_video.py
+++ inference_video.py
@@ -8,18 +8,22 @@ Created on Fri Mar 25 23:28:19 2022
 
 import cv2
 import time
-import glob
 import numpy as np
 import tensorflow.compat.v1 as tf
 
-input_folder = "dataset/images/"
+input_folder = "dataset/source_videos/"
+input_video = "sample_video_18.mp4"
 model_path = "saved_model/saved_model"
-output_folder = "output_folder/"
 
 colors = [(255,0,0), (229, 52, 235), (235, 85, 52),
           (14, 115, 51), (14, 115, 204)]
 
+path = input_folder+input_video
+
+cap = cv2.VideoCapture(path)
 cv2.namedWindow("display", cv2.WINDOW_NORMAL)
+
+video = cv2.VideoWriter(input_video, cv2.VideoWriter_fourcc(*'MP4V'), 30, (512,512))
 
 def process_keypoint(kp, kp_s, h, w, img):
     for i, kp_data in enumerate(kp):
@@ -37,10 +41,9 @@ with tf.Session(graph = tf.Graph()) as sess:
     det_keypoint = graph.get_tensor_by_name("StatefulPartitionedCall:4")
     det_keypoint_score = graph.get_tensor_by_name("StatefulPartitionedCall:3")
     print("Model Loaded")
-    for image_path in glob.glob(input_folder+"*.jpg"):
-        filename = image_path.split("/")[-1]
-        frame = cv2.imread(image_path)
-        if frame is not None:
+    while True:
+        ret, frame = cap.read()
+        if ret:
             frame = cv2.resize(frame,(512,512),interpolation = cv2.INTER_AREA)
             height, width, _ = frame.shape
             image_exp_dims = np.expand_dims(frame, axis=0)
@@ -62,10 +65,12 @@ with tf.Session(graph = tf.Graph()) as sess:
                     cv2.rectangle(frame, p1, p2, (0,255,0), 3)
             frame = process_keypoint(keypoint[0][0], keypoint_score[0], height, width, frame)
             cv2.imshow("display",frame)
-            cv2.imwrite(output_folder+filename, frame)
+            video.write(frame)
             print("Time: ", time.time() - start_time)
             cv2.waitKey(1)
         else:
             print("break")
             break
+cap.release()
+video.release()
 cv2.destroyAllWindows()
